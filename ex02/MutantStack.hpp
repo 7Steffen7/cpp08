b6d285f676026adfbdc3cc19ec482da43d520963
Steffen Parth <sparth@2-C-7.42heilbronn.de>
2024-09-20 17:04:31 +0200

    ex02: provided main works the same with mutant_stack and std::list

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,10 +1,16 @@
 #pragma once
-#include <deque>
+// #include <deque>
 #include <stack>
 
 template<typename T>
 class MutantStack : public std::stack<T> {
 
 public:
-
+	using iterator = typename std::deque<T>::iterator;
+	iterator begin() {
+		return this->c.begin();
+	}
+	iterator end() {
+		return this->c.end();
+	}
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,11 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+#include <stack>
 
 int main()
 {
+std::cout << "Mutant Stack" << std::endl;
 MutantStack<int> mstack;
 mstack.push(5);
 mstack.push(17);
@@ -24,5 +27,28 @@ std::cout << *it << std::endl;
 ++it;
 }
 std::stack<int> s(mstack);
+
+std::cout << "\nstd::list" << std::endl;
+std::list<int> list_compare;
+list_compare.push_back(5);
+list_compare.push_back(17);
+std::cout << list_compare.back() << std::endl;
+list_compare.pop_back();
+std::cout << list_compare.size() << std::endl;
+list_compare.push_back(3);
+list_compare.push_back(5);
+list_compare.push_back(737);
+//[...]
+list_compare.push_back(0);
+std::list<int>::iterator it_list = list_compare.begin();
+ std::list<int>::iterator ite_list = list_compare.end();
+++it_list;
+--it_list;
+while (it_list != ite_list)
+{
+std::cout << *it_list << std::endl;
+++it_list;
+}
+std::list<int> s_list(list_compare);
 return 0;
 }
